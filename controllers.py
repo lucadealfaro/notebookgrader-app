0c8f8076b70003bd8639d0f8ff2545e602f738cf
Luca de Alfaro <luca@dealfaro.org>
2022-03-12 17:41:40 +0100

    fixed fixtures

--- controllers.py
+++ controllers.py
@@ -34,7 +34,7 @@ from .models import get_user_email
 url_signer = URLSigner(session)
 
 @action('index')
-@action.uses(db, auth, 'index.html')
+@action.uses('index.html', db, auth)
 def index():
     return dict(
         # COMPLETE: return here any signed URLs you need.
